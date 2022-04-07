55f0e0b02154fae18d7a6925315055ed3c60cd8f
Gustav Andoff <gustav.andoff@gmail.com>
2022-04-07 11:06:06 +0200

    Change from localhost

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000'
+        origin: `http://172.31.204.128:3000`
     }
 });
 
